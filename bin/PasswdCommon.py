381272449bd92b1cc307bb6632896fb43bc1b951
fcvane@163.com <abc123>
2019-06-12 16:19:20 +0800

    加密&解密

---
+++ bin/PasswdCommon.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/6/12 15:39
+# @Author  : Fcvane
+# @Param   : 加密&解密
+# @File    : PasswdCommon.py
+
+
+import base64
+import sys,os
+from Crypto import Random
+from Crypto.Cipher import AES
+
+# 加密函数
+def encrypt(originalPassword):
+    bs = AES.block_size
+    pad = lambda s: s + (bs - len(s) % bs) * chr(bs - len(s) % bs)
+
+    paddPassword = pad(originalPassword)
+    iv = Random.OSRNG.new().read(bs)
+    key = os.urandom(32)
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    encryptPassword = base64.b64encode(iv + cipher.encrypt(paddPassword) + key)
+    return encryptPassword
+
+# 解密函数
+def decrypt(encryptPassword):
+    base64Decoded = base64.b64decode(encryptPassword)
+    bs = AES.block_size
+    unpad = lambda s: s[0:-s[-1]]
+    iv = base64Decoded[:bs]
+    key = base64Decoded[-32:]
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    originalPassword = unpad(cipher.decrypt(base64Decoded[:-32]))[bs:]
+    return originalPassword
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 3:
+        if sys.argv[1] == '-e':
+            print (encrypt(sys.argv[2]))
+        elif sys.argv[1] == '-d':
+            print (decrypt(sys.argv[2]))
