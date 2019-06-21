4f3ef309c68d536795bbbd4079dbe8b3fe9c4636
fcvane@163.com <abc123>
2019-06-21 09:33:05 +0800

    配置修改

--- bin/DBConnect.py
+++ bin/DBConnect.py
@@ -20,54 +20,57 @@ except ImportError:
 
 name = os.path.basename(__file__)
 
+
 # 获取数据库连接
-def getConnect(program):
-    config = dbCFGInfo(program)
-    for cronName in config.keys():
-        dbType = cronName
-        cronConfig = config[cronName]
-        if cronConfig['enable'] == 'Ture':
-            host = cronConfig['host']
-            port = cronConfig['port']
-            sid = cronConfig['sid']
-            serverName = cronConfig['serverName']
-            userName = cronConfig['userName']
-            passWord = cronConfig['passWord']
-            print(dbType, host, port, sid, serverName, userName, passWord)
-            try:
-                if dbType == 'oracle':
-                    if sid is None or sid == '':
-                        LogUtil.log(name, 'Sid not exists , the %s config is error'.format(program=program), 'error')
-                        sys.exit()
-                    else:
-                        LogUtil.log(name, 'Exec connect oracle start ...', 'info')
-                        print (" cx_Oracle.connect({userName}, {passWord},'{host}:{port}/{sid}')".format(userName=userName,passWord=passWord,host=host, port=port, sid=sid))
-                        connect = cx_Oracle.connect(userName, passWord,
-                                                    '{host}:{port}/{sid}'.format(host=host, port=port, sid=sid))
-                        LogUtil.log(name, 'Connect oracle sucessful !', 'info')
-                elif dbType == 'mysql':
-                    if serverName is None or sid == '':
-                        LogUtil.log(name, 'ServerName not exists , the %s config is error !!'.format(program=program), 'error')
-                        sys.exit()
-                    else:
-                        LogUtil.log(name, 'Exec connect mysql start ...','info')
-                        connect = pymysql.connect(host=host, port=int(port), user=userName, passwd=passWord,
-                                                  db=serverName, charset='utf8')
-                        LogUtil.log(name, 'Connect mysql sucessful !', 'info')
-                elif dbType == 'postgresql':
-                    LogUtil.log(name, 'Exec connect postgresql start ...', 'info')
-                    connect = psycopg2.connect(database=serverName, user=userName, password=passWord, host=host,
-                                               port=port)
-                    LogUtil.log(name, 'Connect postgresql sucessful !', 'info')
-            except:
-                LogUtil.log(name, 'Connect to db failure ,please check config and try again !!', 'error')
-                connect = ""
-            return connect
+def getConnect(auth):
+    config = dbCFGInfo(auth)
+    cronConfig = config[auth]
+    if cronConfig['enable'] == 'Ture':
+        dbType = cronConfig['dbType']
+        host = cronConfig['host']
+        port = cronConfig['port']
+        sid = cronConfig['sid']
+        serverName = cronConfig['serverName']
+        userName = cronConfig['userName']
+        passWord = cronConfig['passWord']
+        # print(dbType, host, port, sid, serverName, userName, passWord)
+        try:
+            if dbType == 'ORACLE':
+                if sid is None or sid == '':
+                    LogUtil.log(name, 'Sid not exists , the %s config is error'.format(program=auth), 'error')
+                    sys.exit()
+                else:
+                    LogUtil.log(name, 'Exec connect oracle start ...', 'info')
+                    # print(" cx_Oracle.connect({userName}, {passWord},'{host}:{port}/{sid}')".format(userName=userName,
+                    #                                                                                 passWord=passWord,
+                    #                                                                                 host=host,
+                    #                                                                                 port=port, sid=sid))
+                    connect = cx_Oracle.connect(userName, passWord,
+                                                '{host}:{port}/{sid}'.format(host=host, port=port, sid=sid))
+                    LogUtil.log(name, 'Connect oracle sucessful !', 'info')
+            elif dbType == 'MYSQL':
+                if serverName is None or sid == '':
+                    LogUtil.log(name, 'ServerName not exists , the %s config is error !!'.format(program=auth),
+                                'error')
+                    sys.exit()
+                else:
+                    LogUtil.log(name, 'Exec connect mysql start ...', 'info')
+                    connect = pymysql.connect(host=host, port=int(port), user=userName, passwd=passWord,
+                                              db=serverName, charset='utf8')
+                    LogUtil.log(name, 'Connect mysql sucessful !', 'info')
+            elif dbType == 'POSTGRESQL':
+                LogUtil.log(name, 'Exec connect postgresql start ...', 'info')
+                connect = psycopg2.connect(database=serverName, user=userName, password=passWord, host=host,
+                                           port=port)
+                LogUtil.log(name, 'Connect postgresql sucessful !', 'info')
+        except:
+            LogUtil.log(name, 'Connect to db failure ,please check config and try again !!', 'error')
+            connect = ""
+        return connect
 
 
 if __name__ == '__main__':
-
-    conn = getConnect('commondb')
+    conn = getConnect('SCOTT_10.45.15.201')
     cur = conn.cursor()
     cur.execute("select 1+1 from dual")
     result = cur.fetchall()
--- bin/XmlUtil.py
+++ bin/XmlUtil.py
@@ -20,42 +20,43 @@ except ImportError:
 格式为：
 <?xml version='1.0' encoding='utf-8'?>
 <configuration>
-    <db type="oracle">
-        <host>10.45.15.20</host>
-        <port>1521</port>
-        <sid>orcl</sid>
-        <serverName></serverName>
-        <userName>scott</userName>
-        <passWord>xoiMDvbRNj8hS8f7QkVdwR5VTVnNHiLBXKEPE/pd2DyYxwW0gVL3VYiw6Mr6hv9+hF5TqD5/WGc4wuolwHup9Q==</passWord>
-        <enable>Ture</enable>
-    </db>
+    <auth db="SCOTT_10.45.15.201">
+        <db type="oracle">
+            <host>10.45.15.20</host>
+            <port>1521</port>
+            <sid>orcl</sid>
+            <serverName></serverName>
+            <userName>scott</userName>
+            <passWord>xoiMDvbRNj8hS8f7QkVdwR5VTVnNHiLBXKEPE/pd2DyYxwW0gVL3VYiw6Mr6hv9+hF5TqD5/WGc4wuolwHup9Q==</passWord>
+            <enable>Ture</enable>
+        </db>
+    </auth>
 </configuration>
 '''
 
 name = os.path.basename(__file__)
 
 
-def dbCFGInfo(program):
-    taskName = VariableUtil.DB_PATH + os.sep + 'db_{program}.xml'.format(program=program)
+def dbCFGInfo(auth):
+    taskName = VariableUtil.DB_PATH + os.sep + 'db_commondb.xml'
     LogUtil.log(name, 'Start to analysis {taskName}'.format(taskName=taskName), 'info')
     result = {}
     try:
         tree = etree.parse(taskName)
         # 获得子元素
-        elemlist = tree.findall('db')
+        elemlist = tree.findall('auth[@id="%s"]' % auth)
         # 遍历task所有子元素
         for elem in elemlist:
             array = {}
             for child in elem.getchildren():
-                # print (child.tag, ":", child.attrib, ":", child.text)
+                # print(child.tag, ":", child.text)
                 if child.tag == "passWord":
                     array[child.tag] = PasswdUtil.decrypt(child.text)
                 else:
                     array[child.tag] = child.text
-            result[elem.attrib['type']] = array
-            # print (array,"-----",result)
-        LogUtil.log(name, 'File {taskName} analysis sucessful "'.format(taskName=taskName), 'info')
-    except Exception as e:  # 捕获除与程序退出sys.exit()相关之外的所有异常
+            result[elem.attrib['id']] = array
+        LogUtil.log(name, 'File {taskName} analysis sucessful '.format(taskName=taskName), 'info')
+    except Exception as e:
         LogUtil.log(name, 'File {taskName} analysis failure '.format(taskName=taskName), 'error')
         sys.exit()
     # print(result)
@@ -63,5 +64,5 @@ def dbCFGInfo(program):
 
 
 if __name__ == '__main__':
-    dbConfig = dbCFGInfo('commondb')
+    dbConfig = dbCFGInfo('SCOTT_10.45.15.201')
     print(dbConfig)
